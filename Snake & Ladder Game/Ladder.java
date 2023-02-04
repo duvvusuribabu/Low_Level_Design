6b2a2899e3c9e3184d31a3fd85c418e3ab990dfc
Suribabu_Duvvu <89627322+duvvusuribabu@users.noreply.github.com>
2023-02-04 21:20:28 +0530

    Ladder class

---
+++ Snake & Ladder Game/Ladder.java
@@ -0,0 +1,15 @@
+ public class Ladder {
+    private int start;
+    private int end;
+    Ladder(int start, int end){
+          this.start = start;
+          this.end = end;
+    }
+    public int getStart(){
+        return start;
+    }
+    public int getEnd(){
+        return end;
+    }
+    
+}
