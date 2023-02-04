17c9891a2c60cd1043b3f279519ae933ec159de8
Suribabu_Duvvu <89627322+duvvusuribabu@users.noreply.github.com>
2023-02-04 21:23:30 +0530

    Player class

---
+++ Snake & Ladder Game/Player.java
@@ -0,0 +1,24 @@
+  public class Player{
+    private String playerName;
+    private int position;
+
+  Player(String playerName){
+    this.playerName = playerName;        //constructor
+    this.position=0;
+  }
+  public String getPlayerName()
+  {
+    return playerName;
+  }
+  public int getPosition()
+  {
+    return position;
+  }
+ public void setPosition(int position)
+ {
+    this.position = position;
+ }
+ }
+
+    
+
